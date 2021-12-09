fd9e1a063024e657d3d34cf26ab38a68350d7d26
OskarDubas-dev <oohdee.dev@gmail.com>
2021-12-09 13:36:31 +0000

    Tank "turn" and "move" implemented inside blackboard

--- Source/ToonTanks/EnemyTank.cpp
+++ Source/ToonTanks/EnemyTank.cpp
@@ -2,4 +2,17 @@
 
 
 #include "EnemyTank.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
+AEnemyTank::AEnemyTank()
+{
+	FloatingPawnMovement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("Floating Pawn Movement"));
+	
+}
+
+/*void AEnemyTank::Turn(float Value)
+{
+	DeltaRotation.Yaw = (Value * UGameplayStatics::GetWorldDeltaSeconds(this) * TurnSpeed);
+	AddActorLocalRotation(DeltaRotation, true);
+}*/
--- Source/ToonTanks/EnemyTank.h
+++ Source/ToonTanks/EnemyTank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "BasePawn.h"
+#include "GameFramework/FloatingPawnMovement.h"
 #include "EnemyTank.generated.h"
 
 /**
@@ -13,5 +14,22 @@ UCLASS()
 class TOONTANKS_API AEnemyTank : public ABasePawn
 {
 	GENERATED_BODY()
+
+	AEnemyTank();
+public:
+	UFUNCTION(BlueprintImplementableEvent)
+	void Turn(float Value);
+
+private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables", meta = (AllowPrivateAccess = "true"))
+	float MoveSpeed = 100.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variables", meta = (AllowPrivateAccess = "true"))
+	float TurnSpeed = 100.0f;
+	
+	FVector DeltaLocation{0.0f};
+	FRotator DeltaRotation{0.0f};
+	UPROPERTY(VisibleAnywhere, Category = "AI")
+	class UFloatingPawnMovement* FloatingPawnMovement;
 	
 };
