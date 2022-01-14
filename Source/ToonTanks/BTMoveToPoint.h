44ac082fde83b189f09f3167aa4cb4275cc8a13c
OskarDubas-dev <oohdee.dev@gmail.com>
2022-01-14 10:46:42 +0000

    Patrol AI is working, next step add more functionality to the ai

---
+++ Source/ToonTanks/BTMoveToPoint.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_MoveTo.h"
+#include "BTMoveToPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API UBTMoveToPoint : public UBTTask_MoveTo
+{
+	GENERATED_BODY()
+	virtual 
+	
+};
